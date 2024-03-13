50ef83d2a4b59dad3400b6357da7922958edd4e1
Kamatchi <85495544+Kamatchi-23@users.noreply.github.com>
2024-03-13 19:24:34 +1100

    Update bank.py

--- Labs/lab4/BankApp/bank.py
+++ Labs/lab4/BankApp/bank.py
@@ -10,3 +10,39 @@ NOTES:
 • Handle exceptions related to funds availability in the withdraw action
 • The Bank system should offer repeatedly the choices to customers until x is entered
 """
+
+class Bank:
+    def __init__(self, balance):
+        self.balance = balance
+    def viewBalance(self):
+        print("Starting Balance: $", self.balance)
+    def deposit(self, amount):
+        self.balance += amount
+        print(f"Amount deposited : $ {amount:.2f}")
+    def withdraw(self, amount):
+        if amount > self.balance:
+            print("Insufficient funds")
+        else:
+            self.balance -= amount
+            print(f"Amount withdrawn : $ {amount:.2f}")
+def main():
+    start_balance = 1000
+    bank = Bank(start_balance)
+    menuopt = input("Start banking (s/w/d/x):")
+    while menuopt != 'x':
+        match menuopt:
+            case 's':
+                bank.viewBalance()
+            case 'd':
+                amount = float(input("Amount to deposit: $"))
+                bank.deposit(amount)
+            case 'w':
+                amount = float(input("Amount to withdraw: $"))
+                bank.withdraw(amount)
+            case _:
+                print("Please enter only s, w, d or x only")
+        menuopt = input("Continue banking (s/w/d/x):")
+    print("Good-Bye")
+
+if __name__ == "__main__":
+    main()
